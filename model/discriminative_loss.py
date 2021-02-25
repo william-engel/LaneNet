39c7678ff872067dd53896553b30d8da7ed0e3ac
william-engel <72106479+william-engel@users.noreply.github.com>
2021-02-25 16:58:49 +0100

    Update discriminative_loss.py

--- model/discriminative_loss.py
+++ model/discriminative_loss.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 # Source:
 # https://github.com/hq-jiang/instance-segmentation-with-discriminative-loss-tensorflow/blob/master/loss.py
 
-def discriminative_loss():
+def test():
   pass
