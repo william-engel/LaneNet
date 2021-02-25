67cf786020b3ac54c6a8b664b1e0c1951096fc84
William Engel <william.engel@web.de>
2021-02-25 22:01:35 +0100

    Add eval metric

---
+++ model/evaluate.py
@@ -0,0 +1,23 @@
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
+tf.get_logger().setLevel('INFO')
+
+class MeanIoU_FromLogits(tf.keras.metrics.Metric):
+    def __init__(self, num_classes, name=None, **kwargs):
+        super(MeanIoU_FromLogits, self).__init__(name=name, **kwargs)
+        self.MeanIoU = tf.keras.metrics.MeanIoU(num_classes)
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.nn.softmax(y_pred)
+        y_pred = tf.argmax(y_pred, axis = -1)
+
+        y_true = tf.argmax(y_true, axis = -1)
+
+        return self.MeanIoU.update_state(y_true, y_pred)
+
+    def result(self):
+        return self.MeanIoU.result()
+
+    def reset_states(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.MeanIoU.reset_states()
